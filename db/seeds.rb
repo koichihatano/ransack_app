b78916badf39ab2270c245363ea81e0546b7591f
KOICHI HATANO <hatch.k53@gmail.com>
2020-07-28 11:52:20 +0900

    rails応用6に回答

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "斎藤", age: 34)
+User.create(name: "山田", age: 29)
+User.create(name: "山下", age: 42)
+User.create(name: "新庄", age: 62)
+User.create(name: "三条", age: 52)
+User.create(name: "葛城", age:18)
